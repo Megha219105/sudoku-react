498e5b737650f0dade5147695c4b40d48ed2a753
Megha Malhotra <mma400@uclive.ac.nz>
2020-08-26 21:39:28 +1200

    initial commit

---
+++ src/components/TopNumber.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+const yellow = 'rgb(255, 215, 18)';
+
+export class TopNumber extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 'highest': 0 };
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.number > this.state.highest) {
+      this.setState({
+        'highest': nextProps.number
+      });
+    }
+  }
+  
+  componentWillUpdate(nextProps, nextState) {
+    if (document.body.style.background != yellow 
+	  && this.state.highest >= 950*1000) {
+	  document.body.style.background = yellow;
+	}
+   else if (!this.props.game 
+	  && nextProps.game) {
+	  document.body.style.background = 'white';
+  }
+}
+  
+  render() {
+    return (
+      <h1>
+        Top Number: {this.props.number}
+      </h1>
+    );
+  }
+}
+
+TopNumber.propTypes = {
+  number: PropTypes.number,
+  game: PropTypes.bool
+};
